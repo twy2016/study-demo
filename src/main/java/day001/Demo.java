b5d8171cb16e32835ce3de5ebf1a708645c5ae72
twy2016 <whgp1994@163.com>
2022-05-17 08:16:24 +0800

    build: 初始化项目

---
+++ src/main/java/day001/Demo.java
@@ -0,0 +1,10 @@
+package day001;
+
+/**
+ * final修饰符
+ *
+ * @author gongpeng
+ * @date 2022/5/17 8:08
+ */
+public final class Demo {
+}
---
+++ src/main/java/day001/SonOfDemo.java
@@ -0,0 +1,10 @@
+package day001;
+
+/**
+ * final修饰的类不可继承
+ * @author gongpeng
+ * @date 2022/5/17 8:08
+ */
+public class SonOfDemo //extends Demo
+{
+}
